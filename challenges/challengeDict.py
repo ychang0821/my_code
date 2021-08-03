47042a035ae6354ff895ed4d8ae426e50c23087e
ychang0821 <changyun0821@gmail.com>
2021-08-03 08:50:09 -0700

    super bonus and mega bonus

--- challenges/challengeDict.py
+++ challenges/challengeDict.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
 
 marvelchars= {
-"Starlord":
+"starlord":
   {"real name": "peter quill",
   "powers": "dance moves",
   "archenemy": "Thanos"},
 
-"Mystique":
+"mystique":
   {"real name": "raven darkholme",
   "powers": "shape shifter",
   "archenemy": "Professor X"},
 
-"She-Hulk":
+"she-Hulk":
   {"real name": "jennifer walters",
   "powers": "super strength & intelligence",
   "archenemy": "Titania"}
              }
 
-char_name = input(" Which character do you want to know about? (Starlord, Mystique, She-Hulk)")
+char_name = input(" Which character do you want to know about? (Starlord, Mystique, She-Hulk)").lower()
+
+while marvelchars.get(char_name, "not exist") == "not exist":
+  char_name = input(" Which character do you want to know about? (Starlord, Mystique, She-Hulk)").lower()
 
 char_stat = input(" What statistic do you want to know about? (real name, powers, archenemy)")
+
+while marvelchars.get(char_name).get(char_stat, "not exist") == "not exist":
+  char_stat = input(" What statistic do you want to know about? (real name, powers, archenemy)")
+
 if char_stat == "real name":
   value = marvelchars.get(char_name).get(char_stat).title()
 else:
   value = marvelchars.get(char_name).get(char_stat)
-print(f"{char_name}'s {char_stat} is : {value})")
+print(f"{char_name.title()}'s {char_stat} is : {value}")
