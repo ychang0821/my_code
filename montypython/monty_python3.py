5cf54607c2aff9fd5481b57eabbfb0d46a84aac3
Yun Chang <changyun0821@gmail.com>
2021-08-03 20:41:51 +0000

    reason for commit

--- montypython/monty_python3.py
+++ montypython/monty_python3.py
@@ -2,13 +2,13 @@
 round = 0
 answer = " "
 
-while round < 3 and answer != "Brian":
+while round < 3 and answer not in [ "Brian", "Shrubbery"]:
     round += 1     # increase the round counter by 1
-    answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
-    if answer.capitalize() == "Brian": # logic to check if user gave correct answer
+    answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ').capitalize().strip()
+    if answer == "Brian": # logic to check if user gave correct answer
         print("Correct!")
         break
-    elif answer.capitalize() == "Shrubbery":
+    elif answer == "Shrubbery":
         print("You gave the super secret answer")
         break
     elif round == 3:    # logic to ensure round has not yet reached 3
