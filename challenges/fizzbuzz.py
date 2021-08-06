9520defff317932c474cfd7c545818b62b798bd1
ychang0821 <changyun0821@gmail.com>
2021-08-06 08:08:06 -0700

    fizz buzz added

---
+++ challenges/fizzbuzz.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env pyhton3
+
+def getnumbers():
+    numberlist = []
+    with open("numfile.txt", "r") as numfile:
+        for row in numfile:
+            numberlist.append(int(row))
+    return numberlist
+
+def fizzbuzz(numberlist):
+    Fizzes = 0 
+    Buzzes = 0 
+    FizzBuzzes = 0
+    for num in numberlist:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+            FizzBuzzes += 1
+        elif num % 3 == 0:
+            print("Fizz")
+            Fizzes += 1
+        elif num % 5 == 0:
+            print("Buzz")
+            Buzzes += 1
+        else:
+            print(num)
+    return {"Fizzes": Fizzes, "Buzzes": Buzzes, "FizzBuzzes": FizzBuzzes}
+    
+def main():
+    numberlist = getnumbers()
+    print(fizzbuzz(numberlist))
+
+if __name__ == "__main__":
+    main()
